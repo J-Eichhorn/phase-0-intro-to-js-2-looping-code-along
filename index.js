23a0fd782e438d6e41a386b28cdd4d55b0ba51b5
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-02-21 14:28:12 -0500

    passes looping lap

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+/* const gifts  = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+function writeCards(array, eventName) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newArray
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number--);
+    }
+}
